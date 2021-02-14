dc9f80dc2fdd5ab801f61fa0bcd50f8f08dd5f66
zell <zell@zelltech.com>
2021-02-14 01:13:29 +0100

    fixing cmake

---
+++ Z3DConfig.cmake
@@ -0,0 +1,7 @@
+
+include(CMakeFindDependencyMacro)
+# find_package(Qt5 COMPONENTS Core SerialPort REQUIRED)
+# find_package(Ceres REQUIRED)
+# find_package(Eigen3 REQUIRED)
+# find_package(CSCore CONFIG REQUIRED)
+include("${CMAKE_CURRENT_LIST_DIR}/Z3DTargets.cmake")
